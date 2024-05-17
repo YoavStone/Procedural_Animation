35ae1c70abc6a820c13b348feef67a16dad8cae3
YoavStone <yoav.stone@gmail.com>
2024-05-17 17:06:33 +0300

    body setup almost knee and elbow

--- py_2d_prototype_leg/settings.py
+++ py_2d_prototype_leg/settings.py
@@ -26,7 +26,7 @@ def angle_from_location(loc1, loc2):
     rad = math.atan2((loc2[1] - loc1[1]), (loc2[0] - loc1[0]))
     deg = math.degrees(rad)
     if deg < 0:
-        deg -= 360
+        deg = 360 + deg
     return deg
 
 
