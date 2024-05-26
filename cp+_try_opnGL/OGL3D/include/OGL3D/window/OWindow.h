542c58e24e000b8080cb80f0bb8ec5f851f40a99
YoavStone <yoav.stone@gmail.com>
2024-05-26 15:56:08 +0300

    cpp opngl try

---
+++ cp+_try_opnGL/OGL3D/include/OGL3D/window/OWindow.h
@@ -0,0 +1,14 @@
+//
+// Created by yoavs on 26/05/2024.
+//
+
+#ifndef PROCEDURAL_ANIMATION_OWINDOW_H
+#define PROCEDURAL_ANIMATION_OWINDOW_H
+
+
+class OWindow {
+
+};
+
+
+#endif //PROCEDURAL_ANIMATION_OWINDOW_H
---
+++ cp+_try_opnGL/OGL3D/surce/OGL3D/window/OWindow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yoavs on 26/05/2024.
+//
+
+#include "../../../include/OGL3D/window/OWindow.h"
